8c7e5feca7e4148c81b2d7f259d2faa17d01f7d8
AbhisumatK <kunduabhisumat@gmail.com>
2026-05-31 21:53:34 +0530

    Tested continuous and discrete actions

--- Assets/scripts/MoveToBallAgent.cs
+++ Assets/scripts/MoveToBallAgent.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MoveToBallAgent : Agent
 {
