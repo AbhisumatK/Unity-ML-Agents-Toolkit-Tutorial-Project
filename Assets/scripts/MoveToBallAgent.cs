8fa821bdd6bcc1e5b0b965e8665dbf792bf8375b
AbhisumatK <kunduabhisumat@gmail.com>
2026-06-05 13:37:52 +0530

    Training pace is increased by training multiple (20) environments simultaneously. The agent (Albert) is standing idle to minimise penalty

--- Assets/scripts/MoveToBallAgent.cs
+++ Assets/scripts/MoveToBallAgent.cs
@@ -8,17 +8,20 @@ using System.Collections.Generic;
 public class MoveToBallAgent : Agent
 {
     [SerializeField] private Transform ballTransform; // Reference to the ball's transform
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
 
     public override void OnEpisodeBegin()
     {
         // Reset the agent's position and the ball's position at the beginning of each episode
-        transform.position = new Vector3(-1.85f, 0.52f, 1.48f);
+        transform.localPosition = new Vector3(0.9f, 0.5f, -1.34f);
     }
     public override void CollectObservations(VectorSensor sensor)
     {
         // Example: sensor.AddObservation(transform.position); // Add the agent's position as an observation
-        sensor.AddObservation(transform.position); // Add the agent's position as an observation
-        sensor.AddObservation(ballTransform.position); // Add the ball's position as an observation
+        sensor.AddObservation(transform.localPosition); // Add the agent's position as an observation
+        sensor.AddObservation(ballTransform.localPosition); // Add the ball's position as an observation
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -26,8 +29,10 @@ public class MoveToBallAgent : Agent
         float moveX = actions.ContinuousActions[0]; // Get the horizontal movement action
         float moveZ = actions.ContinuousActions[1]; // Get the vertical movement action
 
-        float moveSpeed = 5f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed; // Move the agent based on the actions
+        float moveSpeed = 3f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed; // Move the agent based on the actions
+
+        
     }
     
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -43,11 +48,13 @@ public class MoveToBallAgent : Agent
         if (other.CompareTag("ball"))
         {
             SetReward(+1f); // Reward the agent for reaching the ball
+            floorMeshRenderer.material = winMaterial; // Change the floor material to indicate a win
             EndEpisode(); // End the episode after reaching the ball
         }
         if (other.CompareTag("wall"))
         {
             SetReward(-1f); // Reward the agent for reaching the ball
+            floorMeshRenderer.material = loseMaterial; // Change the floor material to indicate a loss
             EndEpisode(); // End the episode after reaching the ball
         }
     }
