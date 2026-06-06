77808dd8ba1bb7557afc4d8f41c378e62295b4ec
AbhisumatK <kunduabhisumat@gmail.com>
2026-06-06 13:39:08 +0530

    Changed the spawn location to be random after every turn. but it is still randomized at one half of the environment

--- Assets/scripts/MoveToBallAgent.cs
+++ Assets/scripts/MoveToBallAgent.cs
@@ -15,7 +15,8 @@ public class MoveToBallAgent : Agent
     public override void OnEpisodeBegin()
     {
         // Reset the agent's position and the ball's position at the beginning of each episode
-        transform.localPosition = new Vector3(0.9f, 0.5f, -1.34f);
+        transform.localPosition = new Vector3(Random.Range(1.9f, -0.1f), 0.5f, Random.Range(-4.7f, 1.4f)); // Randomize the agent's starting position
+        ballTransform.localPosition = new Vector3(Random.Range(-3.1f, -0.9f), 0.5f, Random.Range(-4.7f, 1.55f)); // Randomize the ball's starting position
     }
     public override void CollectObservations(VectorSensor sensor)
     {
